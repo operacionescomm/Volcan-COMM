08e9707976ac96f91a1ed127adbf52b6efb0cb38
operacionescomm <operacionescomm@gmail.com>
2026-08-13 10:45:36 -0500

    Map slide 15 to Yauli atenciones

--- config/slides.js
+++ config/slides.js
@@ -12,24 +12,30 @@ const SLIDES = {
     description: 'Atenciones en la Operación - VOLCAN'
   },
   15: {
+    key: 'yauli-atenciones',
+    template: 'yauli-atenciones',
+    active: true,
+    description: 'Cantidad de Atenciones - U.M. Yauli'
+  },
+  16: {
     key: 'top-requerimientos',
     template: 'top-requerimientos',
     active: false,
     description: 'Módulo preparado para siguiente etapa'
   },
-  16: {
+  17: {
     key: 'top-incidentes',
     template: 'top-incidentes',
     active: false,
     description: 'Módulo preparado para siguiente etapa'
   },
-  17: {
+  18: {
     key: 'valorizacion',
     template: 'valorizacion',
     active: false,
     description: 'Módulo preparado para siguiente etapa'
   },
-  18: {
+  19: {
     key: 'top-suministros-costo',
     template: 'top-suministros-costo',
     active: false,
